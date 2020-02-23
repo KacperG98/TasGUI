9f38059881976ca3d5702785f02d84ce82b5757a
KacperG98 <kacper.gmurczyk1998@interia.pl>
2020-02-23 16:24:23 +0100

    project pre structure

---
+++ src/Ticket/Ticket.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Ticket extends Component {
+    constructor(props){
+        super(props)
+        state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Ticket;
